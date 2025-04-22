d810112c7c715d8084e1b3d456fc4329e8547e31
bluezapus <lynx@archlinux.localdomain>
2025-04-22 16:21:41 +0700

    BluezCve

--- BluezCve.sh
+++ BluezCve.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [ ! -f ".venv/bin/activate" ]; then
-  echo "[✘] Virtual environment not found at BluezCve/.venv"
+  echo "Virtual environment not found at BluezCve/.venv"
   exit 1
 fi
 
 source .venv/bin/activate
 
 if ! python -m BluezCve "$@"; then
-  echo "[!] python gagal, mencoba dengan python3..."
+  echo "[!] python command failed, use python3..."
   python3 -m BluezCve "$@"
 fi
--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Buat virtual environment
 python3 -m venv .venv
 
 if [ $? -ne 0 ]; then
@@ -8,17 +7,15 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-echo "Virtual environment .venv berhasil dibuat."
+echo "Virtual environment .venv succesed create."
 
-# Aktifkan virtual environment
 source .venv/bin/activate
 
-# Install dependensi dari requirements.txt
 if [ -f "requirements.txt" ]; then
   echo "Installation from requirements.txt..."
   pip install --upgrade pip
   pip install -r requirements.txt
-  echo "Semua dependensi terinstall."
+  echo "All dependencies were successfully installed ."
 else
   echo "File requirements.txt not found. Pass the package installation."
 fi
